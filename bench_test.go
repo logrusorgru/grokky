08977e8154f5132d152d3d88d952a79c88f5858c
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2017-08-18 11:19:21 +0300

    add simple regexp bench

--- bench_test.go
+++ bench_test.go
@@ -303,3 +303,64 @@ func Benchmark_grokkyVsGrokApacheLog(b *testing.B) {
 	}
 
 }
+
+func Benchmark_simpleNginxAccessLog(b *testing.B) {
+
+	// https://play.golang.org/p/XKtY84Uicf
+
+	// using simple regular expression
+	// (?P<remote_addr>[^\s]+)\s\-\s(?P<remote_user>[^\s]*)\s*\-\s\[(?P<time_local>.+)\]\s\"(?P<request>[^"]*)\"\s(?P<status>[^\s]+)\s(?P<body_bytes_sent>[^\s]+)\s\"(?P<htp_referer>[^"]+)\"\s\"(?P<user_agent>[^"]+)\"
+
+	b.StopTimer()
+
+	h := New()
+
+	h.Must("NSS", `[^\s]*`) // not a space *
+	h.Must("NS", `[^\s]+`)  // not a space +
+	h.Must("NLB", `[^\]]+`) // not a left bracket +
+	h.Must("NQS", `[^"]*`)  // not a quote *
+	h.Must("NQ", `[^"]+`)   // not a double quotes +
+	h.Must("nginx", `%{NS:remote_addr}\s\-\s`+
+		`%{NSS:remote_user}\s*\-\s\[`+
+		`%{NLB:time_local}\]\s\"`+
+		`%{NQ:request}\"\s`+
+		`%{NS:status}\s`+
+		`%{NS:body_bytes_sent}\s\"`+
+		`%{NQ:http_referer}\"\s\"`+
+		`%{NQ:user_agent}\"`)
+
+	nginx, err := h.Get("nginx")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	lines := []string{
+		`66.249.65.159 - - [06/Nov/2014:19:10:38 +0600] ` +
+			`"GET /news/53f8d72920ba2744fe873ebc.html HTTP/1.1" ` +
+			`404 177 "-" ` +
+			`"Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X)` +
+			` AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0` +
+			` Mobile/10A5376e Safari/8536.25 (compatible; Googlebot/2.1;` +
+			` +http://www.google.com/bot.html)"`,
+		`66.249.65.3 - - [06/Nov/2014:19:11:24 +0600] ` +
+			`"GET /?q=%E0%A6%AB%E0%A6%BE%E0%A7%9F%E0%A6%BE%E0%A6%B0 ` +
+			`HTTP/1.1" ` +
+			`200 4223 "-" ` +
+			`"Mozilla/5.0 (compatible; Googlebot/2.1; ` +
+			`+http://www.google.com/bot.html)"`,
+		`66.249.65.62 - - [06/Nov/2014:19:12:14 +0600] ` +
+			`"GET /?q=%E0%A6%A6%E0%A7%8B%E0%A7%9F%E0%A6%BE HTTP/1.1" ` +
+			`200 4356 "-" ` +
+			`"Mozilla/5.0 (compatible; Googlebot/2.1; ` +
+			`+http://www.google.com/bot.html)"`,
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for _, input := range lines {
+			global = nginx.Parse(input)
+		}
+	}
+
+	b.ReportAllocs()
+}
---
+++ simplify_bench_test.go
@@ -0,0 +1,3 @@
+package grokky
+
+// todo
